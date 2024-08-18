1c4355d3d8bdcebdfde817088a3b2ee6fe103193
Yo <yo@yo.yo>
2024-08-18 23:46:52 +0200

    Remove hearts pattern

--- src/layers/patterns/mod.rs
+++ src/layers/patterns/mod.rs
@@ -1,13 +1,9 @@
 use crate::layers::Layer;
-pub use hearts::HeartsPattern;
 use random::Random;
-
-pub mod hearts;
 
 pub fn random_pattern(random: &mut Random) -> Box<dyn Layer> {
     let variant = random.roll::<u8>(1);
     match variant {
-        0 => Box::new(HeartsPattern),
         _ => panic!("Unknown element variant"),
     }
 }
