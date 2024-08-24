6810e7c00e57d7249564515e33491889dd24015c
Yo <yo@yo.yo>
2024-08-25 00:03:20 +0200

    Make some backgrounds less common

--- src/layers/backgrounds/mod.rs
+++ src/layers/backgrounds/mod.rs
@@ -22,13 +22,13 @@ pub fn random_background(random: &mut Random) -> Box<dyn Layer> {
     // Layers and their weights
     let available_layers: Vec<(Box<dyn Layer>, u32)> = vec![
         (Box::new(BackgroundRectangle), 100),
-        (Box::new(BackgroundTwoStripes), 50),
+        (Box::new(BackgroundTwoStripes), 25),
         (Box::new(BackgroundDiagonalSplit), 100),
         (Box::new(BackgroundStraightSplit), 100),
         (Box::new(BackgroundFourSquares), 100),
         (Box::new(BackgroundThreeWaySplit), 100),
         (Box::new(BackgroundDoubleDiagonalSplit), 25),
-        (Box::new(BackgroundCheckerboard), 25),
+        (Box::new(BackgroundCheckerboard), 15),
     ];
 
     // Pick a random layer
