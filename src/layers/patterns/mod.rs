bc4a25a70a604f465d2ee5ee5fbf2d95614d3028
Yo <yo@yo.yo>
2024-08-25 21:46:26 +0200

    Completely remove patterns

--- src/layers/patterns/mod.rs
+++
@@ -1,9 +0,0 @@
-use crate::layers::Layer;
-use random::Random;
-
-pub fn random_pattern(random: &mut Random) -> Box<dyn Layer> {
-    let variant = random.roll::<u8>(1);
-    match variant {
-        _ => panic!("Unknown element variant"),
-    }
-}
