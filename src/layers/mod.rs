649ec180c34e4e5fac9678e340e50dcccd81745a
Yo <yo@yo.yo>
2024-08-25 21:48:19 +0200

    Completely remove patterns

--- src/layers/mod.rs
+++ src/layers/mod.rs
@@ -2,12 +2,10 @@ pub mod backgrounds;
 pub mod big_elements;
 pub mod frames;
 pub mod layer;
-pub mod patterns;
 pub mod small_elements;
 
 pub use backgrounds::*;
 pub use big_elements::*;
 pub use frames::*;
 pub use layer::Layer;
-pub use patterns::*;
 pub use small_elements::*;
