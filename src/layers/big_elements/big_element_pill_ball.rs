499ae99f9a380d936cf62efe0110fcb5dc60a72c
Yo <yo@yo.yo>
2024-09-11 20:00:25 +0200

    Add exclusions

--- src/layers/big_elements/big_element_pill_ball.rs
+++ src/layers/big_elements/big_element_pill_ball.rs
@@ -163,6 +163,7 @@ impl Layer for BigElementPillBall {
             small_elements::small_element_ball::SmallElementBall.type_id(),
             small_elements::small_element_double_circle::SmallElementDoubleCircle.type_id(),
             small_elements::small_element_double_diamond::SmallElementDoubleDiamond.type_id(),
+            small_elements::small_element_cross_shadow::SmallElementCrossShadow.type_id(),
             overlays::overlay_triangle::OverlayTriangle.type_id(),
             overlays::overlay_half_circle::OverlayHalfCircle.type_id(),
         ]
--- src/layers/big_elements/big_element_pill_split_circle.rs
+++ src/layers/big_elements/big_element_pill_split_circle.rs
@@ -164,6 +164,7 @@ impl Layer for BigElementPillSplitCircle {
             small_elements::small_element_ball::SmallElementBall.type_id(),
             small_elements::small_element_double_circle::SmallElementDoubleCircle.type_id(),
             small_elements::small_element_double_diamond::SmallElementDoubleDiamond.type_id(),
+            small_elements::small_element_cross_shadow::SmallElementCrossShadow.type_id(),
             overlays::overlay_triangle::OverlayTriangle.type_id(),
             overlays::overlay_half_circle::OverlayHalfCircle.type_id(),
         ]
