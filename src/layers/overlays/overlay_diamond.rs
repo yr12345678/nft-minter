c37f0ff9cb6727d8aab364719b03c109732317ff
Yo <yo@yo.yo>
2024-09-11 21:29:43 +0200

    Allow diamond overlay to be black

--- src/layers/overlays/overlay_diamond.rs
+++ src/layers/overlays/overlay_diamond.rs
@@ -11,24 +11,51 @@ impl Layer for OverlayDiamond {
         // Generate the areas
         let mut diamond = Polygon::new().set("points", "0,500 500,0 1000,500 500,1000");
 
+        // Pick a color
+        let (color1, color2) = match random.roll::<u8>(2) {
+            0 => {
+                // White
+                (
+                    HSL {
+                        hue: 0,
+                        saturation: 100,
+                        lightness: 100,
+                        opacity: 0,
+                    },
+                    HSL {
+                        hue: 0,
+                        saturation: 100,
+                        lightness: 100,
+                        opacity: 100,
+                    }
+                )            
+            },
+            1 => {
+                // Black
+                (
+                    HSL {
+                        hue: 0,
+                        saturation: 100,
+                        lightness: 0,
+                        opacity: 0,
+                    },
+                    HSL {
+                        hue: 0,
+                        saturation: 100,
+                        lightness: 0,
+                        opacity: 80,
+                    }
+                )            
+            },
+            _ => panic!("No matching overlay color type found!")
+        };
+
         // Set the gradient
         let (gradient, gradient_name) = radial_gradient_definition(
             random,
             None,
-            HSL {
-                // Opacity = 0
-                hue: 0,
-                saturation: 100,
-                lightness: 100,
-                opacity: 0,
-            },
-            HSL {
-                // Opacity = 0
-                hue: 0,
-                saturation: 100,
-                lightness: 100,
-                opacity: 100,
-            },
+            color1,
+            color2
         );
 
         diamond = diamond.set("fill", format!("url(#{gradient_name})"));
